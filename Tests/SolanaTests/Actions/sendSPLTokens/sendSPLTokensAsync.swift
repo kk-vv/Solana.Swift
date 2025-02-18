fbeaacf299d076732bff7c459f42b66eccb11a21
Felix.Hu <hulj1204@yahoo.com>
2025-02-18 16:22:36 +0800

    [U] Update send spl tests

--- Tests/SolanaTests/Actions/sendSPLTokens/sendSPLTokensAsync.swift
+++ Tests/SolanaTests/Actions/sendSPLTokens/sendSPLTokensAsync.swift
@@ -9,19 +9,19 @@ class sendSPLTokensAsync: XCTestCase {
     var signer: Signer!
 
     override func setUpWithError() throws {
-        let wallet: TestsWallet = .devnet
+        let wallet: TestsWallet = .mainnetBeta
         solana = Solana(router: NetworkingRouter(endpoint: endpoint))
-        signer = HotAccount(phrase: "final prevent open stamp loyal exile history window arrest right neutral useless".components(separatedBy: " "))!
+      signer = HotAccount(phrase: wallet.testAccount.components(separatedBy: " "))!
     }
     
     func testSendSPLTokenWithFee() async throws {
         let mintAddress = "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"
-        let source = "CUj17uZgdVVzM9kq2dPqzrCLRgTxK5fKZDgX6tY4ijAu"
+        let source = "5n3vrofk2Cj2zEUm7Bq4eT6GNbw8Hyq8EFdWJX2yXPbh"
         let destination = [
           "GQUcpeAgeiBNKWvPCcG2SBEcrXRs9RYbXUjSZnNbkNg3",
-           "4nZeaVR96o4G9fwZ3efT3cGTN3n2kjRUhXqAGMrUnPVt",
-           "GB3brArmNaPREG6Cp6YHCuW4uimvPv1VbTu3naiinQi8",
-           "2q3EXnRMZx3FBF9nXmb8dMB4MjTQeVudwyj5RKESvzkp"
+          "4nZeaVR96o4G9fwZ3efT3cGTN3n2kjRUhXqAGMrUnPVt",
+          "GB3brArmNaPREG6Cp6YHCuW4uimvPv1VbTu3naiinQi8",
+          "2q3EXnRMZx3FBF9nXmb8dMB4MjTQeVudwyj5RKESvzkp"
         ]
 
         let transactionId = try await solana.action.sendSPLTokens(
